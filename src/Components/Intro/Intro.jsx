f2222b9dac5f0fc27e3771d872d42621ff128f17
VERMAMANISHKUMAR <manishkumarverma091@gmail.com>
2024-09-13 20:07:24 +0530

    same changes

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Intro.css'
 import FloatingDiv from '../FloatingDiv/FloatingDiv'
-
 import {motion} from 'framer-motion'
-
+// left side image
+import Github from '../../images/img/github.png'
+import Linkedin from '../../images/img/linkedin.png'
+import Instagram from '../../images/img/instagram.png'
+import Naukri    from '../../images/img/naukri.png'
+import Coding_Ninja from '../../images/img/coding logo.png'
+//right side image
+import Vector1 from '../../images/img/Vector1.png'
+import Vector2 from '../../images/img/Vector2.png'
+import Boy from '../../images/img/boy.png'
+import Glassesimoji from '../../images/img/glassesimoji.png'
+import Thumbup from '../../images/img/thumbup.png'
 
 const Intro = () => {
   const transition={duration :2, type:'spring'}
@@ -12,18 +22,18 @@ const Intro = () => {
     {/* left side section */}
     <div className='i-left'>
     <div className='i-name'>
-          <span>Hy! I </span>
+          <span>Hy! I Am</span>
           <span>Manish Kumar Verma</span>
           <span>Frontend Developer with high level of experience in web designing and development, <br/>producting the Quality work in <br/>Seeking Full Stack Web Developer Role Proficient in HTML5, CSS, Bootstrap, <br/>JavaScript, React, Node.js, Express, MySQL, WordPress, and PHP.</span>
     </div>
     <button className=' button i-button'>Hire me</button>
     <div className='i-icons'>
     
-    <a href="https://github.com/VERMAMANISHKUMAR"><img src="img/github.png" alt="icon is missing"/></a>
-    <a href="https://www.linkedin.com/in/manishverma25/"> <img src="img/linkedin.png" alt="icon is missing"/></a>
-    <a href="https://www.instagram.com/manish.kumarverma.90663894/"><img src="img/instagram.png" alt="icon is missing"/></a>
-    <a href="https://www.naukri.com/mnjuser/profile?id=&altresid"><img src="img/naukri.png" alt="icon is missing" /></a>
-    <a href="https://www.naukri.com/mnjuser/profile?id=&altresid"><img src="img/coding logo.jpg" alt="icon is missing" /></a>
+    <a href="https://github.com/VERMAMANISHKUMAR"><img src={Github} alt="icon is missing"/></a>
+    <a href="https://www.linkedin.com/in/manishverma25/"> <img src={Linkedin} alt="icon is missing"/></a>
+    <a href="https://www.instagram.com/manish.kumarverma.90663894/"><img src={Instagram} alt="icon is missing"/></a>
+    <a href="https://www.naukri.com/mnjuser/profile?id=&altresid"><img src={Naukri} alt="icon is missing" /></a>
+    <a href="https://www.naukri.com/code360/profile/manishverma"><img src={Coding_Ninja} alt="icon is missing" /></a>
 
     </div>
     </div>
@@ -32,15 +42,15 @@ const Intro = () => {
     {/* right side section */}
 
     <div className='i-right'>
-    <img src="image/Vector1.png" alt="icon is missing"/>
-    <img src="img/Vector2.png" alt="icon is missing"/>
-    <img src="img/boy.png" alt="icon is missing"/>
+    <img src={Vector1} alt="icon is missing"/>
+    <img src={Vector2} alt="icon is missing"/>
+    <img src={Boy} alt="icon is missing"/>
     <motion.img 
     initial={{left:'-36%'}}
     whileInView={{left:'-24%'}}
     transition={transition}
     
-    src="img/glassesimoji.png" alt="icon is missing" className='glassesimoji'/>
+    src={Glassesimoji} alt="icon is missing" className='glassesimoji'/>
          <div>
           <FloatingDiv/>
           </div>
@@ -49,18 +59,14 @@ const Intro = () => {
           whileInView={{ left: '14rem' }}
           transition={transition}
           className='thumbup'>
-       <img src="img/thumbup.png" alt="icon is missing" className='thum'/>
+       <img src={Thumbup} alt="icon is missing" className='thum'/>
        <span>
           Best Desing <br/>
           Award
       </span>
 
      </motion.div>
-     {/* Blur divs */}
-     <div className='blur'style={{background:"rgb(238 210 255)"}}>
-     </div>
-     <div className='blur'style={{background:'#C1F5FF',top:'17rem',width:'21rem',height:'11rem',left:'-9rem'}}></div>
-    
+     
     </div>
     {/* right side section exit */}
     </div>
